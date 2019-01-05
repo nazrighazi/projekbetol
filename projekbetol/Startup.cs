8d1bef016827bd92e4aa192e19d0176556dc3a35
Nazri Ghazi <nazrighazi02@gmail.com>
2019-01-05 12:08:46 +0800

    Add project files.

---
+++ projekbetol/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projekbetol.Startup))]
+namespace projekbetol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
